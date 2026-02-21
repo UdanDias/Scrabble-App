efd5bd3850fcd6256e95f3a80f3bd04ec581073d
UdanDias <udandias892@gmail.com>
2026-02-21 06:33:02 +0530

    added webmvc configuration temporarily before implementing spring security with corsConfigurationSource

--- src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/ScrabbleappApplication.java
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/ScrabbleappApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableWebMvc
 @EntityScan(basePackages = "lk.kelaniya.uok.scrabble.scrabbleapp.entity")
 public class ScrabbleappApplication {
 	@Bean
---
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/config/CORSConfig.java
@@ -0,0 +1,42 @@
+package lk.kelaniya.uok.scrabble.scrabbleapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class CORSConfig {
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource(){
+//        var corsConfiguration=new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+//        corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+//        corsConfiguration.setAllowedHeaders(List.of("*")); //check this one
+//        corsConfiguration.setExposedHeaders(List.of( "Content-Type"));
+//        corsConfiguration.setAllowCredentials(true);
+//
+//        var source=new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**",corsConfiguration);
+//        return source;
+//    }
+
+
+        @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")   //.allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PATCH", "DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
