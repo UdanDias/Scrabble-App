21c33296c630c393ddec26abe96a70f079fda54c
UdanDias <udandias892@gmail.com>
2026-02-24 19:52:58 +0530

    Create web security configuration

--- src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/security/WebSecurityConfig.java
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/security/WebSecurityConfig.java
@@ -1,4 +1,65 @@
 package lk.kelaniya.uok.scrabble.scrabbleapp.security;
 
+import lk.kelaniya.uok.scrabble.scrabbleapp.config.CORSConfig;
+import lk.kelaniya.uok.scrabble.scrabbleapp.security.jwt.AuthEntryPoint;
+import lk.kelaniya.uok.scrabble.scrabbleapp.security.jwt.AuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
 public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final AuthEntryPoint authEntryPoint;
+    private final AuthFilter authFilter;
+    private final CORSConfig corsConfig;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        var dao=new DaoAuthenticationProvider();
+        dao.setUserDetailsService(userDetailsService);
+        dao.setPasswordEncoder(passwordEncoder());
+        return dao;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider daoAuthenticationProvider) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated());
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
