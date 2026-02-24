8088b7d8e2a88961bf2e6e95e36b262d68122fc3
UdanDias <udandias892@gmail.com>
2026-02-24 20:19:55 +0530

    create AuthserviceImpl and UtilData class updated

--- src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/service/impl/secure/AuthServiceImpl.java
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/service/impl/secure/AuthServiceImpl.java
@@ -1,24 +1,47 @@
 package lk.kelaniya.uok.scrabble.scrabbleapp.service.impl.secure;
 
 import jakarta.transaction.Transactional;
+import lk.kelaniya.uok.scrabble.scrabbleapp.dao.secure.UserDao;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.JWTAuthResponse;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.SignIn;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.UserDTO;
+import lk.kelaniya.uok.scrabble.scrabbleapp.entity.security.UserEntity;
+import lk.kelaniya.uok.scrabble.scrabbleapp.security.jwt.JWTutils;
 import lk.kelaniya.uok.scrabble.scrabbleapp.service.secure.AuthService;
+import lk.kelaniya.uok.scrabble.scrabbleapp.util.EntityDTOConvert;
+import lk.kelaniya.uok.scrabble.scrabbleapp.util.UtilData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final UserDao userDao;
+    private final JWTutils jwtutils;
+    private final PasswordEncoder passwordEncoder;
+    private final EntityDTOConvert entityDTOConvert;
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public JWTAuthResponse signIn(SignIn signIn) {
-        return null;
+         authenticationManager.authenticate(
+                 new UsernamePasswordAuthenticationToken(signIn.getEmail(), signIn.getPassword()));
+        var userByEmail = userDao.findByEmail(signIn.getEmail()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var generateToken=jwtutils.generateToken(userByEmail.getEmail(),userByEmail.getAuthorities());
+        return JWTAuthResponse.builder().token(generateToken).build();
     }
 
     @Override
     public JWTAuthResponse signUp(UserDTO userDTO) {
-        return null;
+        userDTO.setUserId(UtilData.generateUserId());
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        var savedUser = userDao.save(entityDTOConvert.convertUserDTOToUserEntity(userDTO));
+        var generatedToken = jwtutils.generateToken(savedUser.getEmail(), savedUser.getAuthorities());
+        return JWTAuthResponse.builder().token(generatedToken).build();
     }
 }
