692e7f0fdcd13f3548a6eb9c20c426875ccc691b
UdanDias <udandias892@gmail.com>
2026-02-24 20:24:21 +0530

    passed data from auth controller to auth service layer

--- src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/controller/secure/AuthController.java
+++ src/main/java/lk/kelaniya/uok/scrabble/scrabbleapp/controller/secure/AuthController.java
@@ -3,6 +3,7 @@ package lk.kelaniya.uok.scrabble.scrabbleapp.controller.secure;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.JWTAuthResponse;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.SignIn;
 import lk.kelaniya.uok.scrabble.scrabbleapp.dto.secure.UserDTO;
+import lk.kelaniya.uok.scrabble.scrabbleapp.service.secure.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
+    private final AuthService authService;
     @PostMapping("/signin")
     public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn)  {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+
+        return new ResponseEntity<>(authService.signIn(signIn),HttpStatus.OK);
     }
 
     @PostMapping("/signup")
     public ResponseEntity<JWTAuthResponse> signUp(@RequestBody UserDTO signUp)  {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(authService.signUp(signUp),HttpStatus.CREATED);
     }
 }
